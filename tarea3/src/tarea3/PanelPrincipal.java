2eac58b64bc48107868e84d282d4fdceb1ba9a75
Alex Jara <alejara2021@udec.cl>
2022-11-13 02:41:47 -0300

    test coin image label
    only a test

--- tarea3/src/tarea3/PanelPrincipal.java
+++ tarea3/src/tarea3/PanelPrincipal.java
@@ -4,25 +4,27 @@ import java.awt.*;
 import javax.swing.*;
 
 public class PanelPrincipal extends JPanel {
+    
     private Comprador com;
     private Expendedor exp;
+    
     public PanelPrincipal() throws NoHayBebidaException, PagoInsuficienteException, PagoIncorrectoException{
         //Por ahora le pongo valores por defecto, pero debe ser modificado.
         exp = new Expendedor(10, 1000, 10, 10);
         Moneda1000 m = new Moneda1000();
         com = new Comprador(m, 2, exp, 10, 10);
-        this.setBackground(Color.black);
-        
-        
+        this.setBackground(Color.black);        
     }
+    
     public void paintComponent(Graphics g) {
-      super.paintComponent(g);
+        
+        super.paintComponent(g);
       
       
-      com.paintComponent(g);
+        com.paintComponent(g);
       
       
-      exp.paintComponent(g);
+        exp.paintComponent(g);
       
       
    }
--- tarea3/src/tarea3/Ventana.java
+++ tarea3/src/tarea3/Ventana.java
@@ -2,14 +2,64 @@
 package tarea3;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.JFrame;
 
 public class Ventana extends JFrame {
+    
     public Ventana(){
+        
         super();
         this.setLayout(new BorderLayout());
         this.setTitle("Tarea 3");
         this.setSize(1500, 800);
+        this.getContentPane().setBackground(new Color(0,0,0));
+        
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        //Windows icon
+        ImageIcon icon = new ImageIcon("icon.png");
+        this.setIconImage(icon.getImage());
+        
+        JLabel dLabel = new JLabel();
+        dLabel.setText("Dispenser");
+        dLabel.setForeground(Color.white);
+        ImageIcon dispenser = new ImageIcon("dispenser.png");
+        dLabel.setIcon(dispenser);
+        
+        //dLabel.setHorizontalAlignment(JLabel.CENTER);
+        //dLabel.setVerticalAlignment(JLabel.CENTER);
+        
+        ImageIcon coin = new ImageIcon("coin.png");        
+        JLabel c100Label = new JLabel();
+        c100Label.setVisible(true);                
+        c100Label.setText(" X 4");
+        c100Label.setForeground(Color.white);                
+        c100Label.setIcon(coin);
+        c100Label.setLocation(0,0);
+        c100Label.setSize(40,40);
+        
+        //ImageIcon coin = new ImageIcon("coin500.png");
+        JLabel c500Label = new JLabel();
+        c100Label.setVisible(true);
+        //c500Label.setBounds(100, 0, 40, 40);        
+        c500Label.setText(" X 6");
+        c500Label.setForeground(Color.white);                
+        c500Label.setIcon(coin);
+        c100Label.setLocation(80,0);
+        c100Label.setSize(40,40);
+        
+        
+                                                
+        this.add(c100Label);
+        this.add(c500Label);
+        
+        //this.add(dLabel);
+        
+                        
         this.setVisible(true);
+        
+                         
+                                        
     }
+    
 }
